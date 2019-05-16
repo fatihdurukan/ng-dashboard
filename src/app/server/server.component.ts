8c0889fed8c205c8111148ea96da4c641fa43082
Fatih Durukan <fatihdurukan42@gmail.com>
2019-05-16 13:51:28 -0500

    Updated the server component cards

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,9 +11,30 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server ;
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if(isOnline){
+      this.serverInput.isOnline = true;
+      this.color = '#66BA6A';
+      this.buttonText = 'Shut Down';
+    }else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerStatus(!this.serverInput.isOnline);
   }
 
 }
